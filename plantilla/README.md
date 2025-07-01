f592aa91ed1e5ef47ad08f99ca6cdadb9e5820bf
Vicente Cao <139924476+vicent-cao02@users.noreply.github.com>
2025-07-01 11:41:06 -0400

    delete plantilla

--- plantilla/README.md
+++
@@ -1,1 +0,0 @@
-# ElectroGalindez.github.io
--- plantilla/store/js/nostra.js
+++
@@ -1,4 +0,0 @@
-document.querySelector('.cart-btns a[href="/checkout/"]').addEventListener('click', function(e) {
-    e.preventDefault(); // Evita el comportamiento predeterminado del enlace
-    window.location.href = '/store/checkout.html'; // Redirige a la página
-});
